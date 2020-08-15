908c5a4d4f2ef10c2d7de961d45fca4b665aa52a
yznaisy <740161192@qq.com>
2020-08-15 17:29:38 +0800

    2020.8.15

--- html-css-js/js/closure/1.php
+++ html-css-js/js/closure/1.php
@@ -1,11 +1,18 @@
 <?php
+/*
+ * @Author       : yznaisy
+ * @Date         : 2020-05-12 13:57:18
+ * @LastEditors  : yznaisy
+ * @LastEditTime : 2020-08-14 15:02:42
+ * @FilePath     : \Code\html-css-js\js\closure\1.php
+ */
 function hd()
 {
-    $n = 1;
-    function sum()
-    {
-        echo $n;
-    };
-    sum();
+  $n = 1;
+  function sum()
+  {
+    echo $n;
+  };
+  sum();
 }
 hd();
