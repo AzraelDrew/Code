96633ab7a9587bea2af42b443900750eca8f3c37
yznaisy <740161192@qq.com>
2020-09-17 22:35:12 +0800

    2020.9.17

--- cs.js
+++ cs.js
@@ -2,10 +2,11 @@
  * @Author       : yznaisy
  * @Date         : 2020-07-02 22:45:50
  * @LastEditors  : yznaisy
- * @LastEditTime : 2020-07-02 23:11:48
+ * @LastEditTime : 2020-09-16 19:13:11
  * @FilePath     : \Code\cs.js
  */
 let str1 = "jspang.com";
 let gogo = function (str) {
-    return `Hi ${str}`;
+  return `Hi ${str}`;
 }
+console.log(str1);
