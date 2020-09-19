e925a66d52f348184037e03fedc2cd3b2a4ed87b
yznaisy <740161192@qq.com>
2020-09-19 23:25:09 +0800

    2020.9.19

--- 1.py
+++
@@ -1,9 +0,0 @@
-'''
- $ @Author       : yznaisy
- $ @Date         : 2020-09-10 15:35:06
- $ @LastEditors  : yznaisy
- $ @LastEditTime : 2020-09-17 13:32:38
-'''
-
-for i in range(5):
-    print(i)
--- 2.py
+++
@@ -1,7 +0,0 @@
-'''
- $ @Author       : yznaisy
- $ @Date         : 2020-09-18 00:24:23
- $ @LastEditors  : yznaisy
- $ @LastEditTime : 2020-09-18 00:24:33
-'''
-print("adwfgasdf")
