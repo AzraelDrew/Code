0c4d8011ce502621c8b305d7b0879cd6bade132d
yznaisy <740161192@qq.com>
2020-09-22 23:57:34 +0800

    2020.9.22

--- Augmented/cs.js
+++ Augmented/cs.js
@@ -2,11 +2,11 @@
  * @Author       : yznaisy
  * @Date         : 2020-07-02 22:45:50
  * @LastEditors  : yznaisy
- * @LastEditTime : 2020-09-20 16:38:28
+ * @LastEditTime : 2020-09-22 16:41:07
  * @FilePath     : \Code\cs.js
  */
-let str1 = "jspang.com";
+let str1 = 'jspang.com';
 let gogo = function (str) {
-  return `Hi ${str}`;
-}
+	return `Hi ${str}`;
+};
 console.log(str1);
--- markdown/Git/git基础.md
+++ markdown/Git/git基础.md
@@ -1,8 +1,16 @@
+> 添加仓库
+
+- git remote add <本地仓库名称> <你的项目地址>
+
 > 修改仓库名称
 
-- git remote rename origin yznaisy
+- git remote rename <原仓库名称> <新仓库名称>
 
 > 修改仓库地址
 
-- git remote set-uel 本地仓库名称 仓库地址
+- git remote set-uel <本地仓库名称> <仓库地址>
 
+
+> git push -u origin master
+
+- 如果当前分支与多个主机存在追踪关系，则可以使用-u选项指定一个默认主机，这样后面就可以不加任何参数使用git push
