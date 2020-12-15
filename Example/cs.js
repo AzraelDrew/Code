e3e8b8884ff39b3e2a8006e144126baaf2af6bf0
Azrael <740161192@qq.com>
2020-12-15 21:25:41 +0800

    2020.12.15

--- Example/cs.js
+++ Example/cs.js
@@ -5,8 +5,23 @@
  * @LastEditTime : 2020-09-22 16:41:07
  * @FilePath     : \Code\cs.js
  */
-let str1 = 'jspang.com';
-let gogo = function (str) {
-	return `Hi ${str}`;
-};
-console.log(str1);
+// let str1 = 'jspang.com';
+// let gogo = function (str) {
+// 	return `Hi ${str}`;
+// };
+// console.log(str1);
+let colors
+// let score = prompt("请输入成绩")
+let score = 1200
+let variants = [1200, 1350, 1500, 1700, 1900, 2050, 2200, 2600]
+let colorsArr = ["red", "green", "black", "white", "cyan", "blue", "yellow"]
+if (score) {
+  let ras = []
+  for (let key in variants) {
+    ras.push(variants[key] >= score)
+  }
+  let ors = ras.indexOf(true)
+  colors = colorsArr[ors]
+  console.log(ras);
+}
+console.log(colors);
